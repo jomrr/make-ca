e5dda561f687cb90de7c6bde3c3abee25ee8762f
Jonas Mauer <jam@kabelmail.net>
2022-10-25 12:13:29 +0200

    feat: make DNs configurable

--- settings.mk
+++ settings.mk
@@ -9,7 +9,9 @@ export DEFAULT_BITS		?= 4096
 # default settings for hash in cnf files
 export DEFAULT_MD		?= sha512
 # base URL of pki, where WEBDIR is found, also used as AIA and CDP base
-export PKIURL			:= http://pki.example.com
+export BASE_URL			:= http://pki.example.com
+# OCSP URL for Online Certificate Status Protocol
+export OCSP_URL			:= http://ocsp.example.com
 # list of CRL Distribution Points in SSH syntax for deploy-crls target (TODO)
 export CDP				:= cdp1:/var/www/pki
 # CA Keys: param for openssl genpkey -algorithm $(CAK_ALG)
